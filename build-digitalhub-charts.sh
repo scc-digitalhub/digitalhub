44d02d7ad1a9c51a56033d3b6640ef1da6c2d719
ffais <ffais@fbk.eu>
2024-04-23 13:15:39 +0200

    fix(digitalhub): fix core init image
    Signed-off-by: ffais <ffais@fbk.eu>

---
+++ build-digitalhub-charts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+build_chart () {
+  array=('aac' 'digitalhub' 'digitalhub-crds')
+  for f in charts/*
+  do
+  IFS='/'
+  read -ra newarr <<< "$f"
+  chart="${newarr[1]}"
+    if ! [[ ${array[*]} =~ $chart ]]; then 
+      helm package "$f" -d charts/digitalhub/charts
+    fi
+  done
+}
+
+build_chart
