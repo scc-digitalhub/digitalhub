fa57467400ffb99252b8be6b9666de9bdf894dc0
Calcagiara <jacopocristoforitn@gmail.com>
2024-07-12 13:47:46 +0200

    feat(dashboard): added support for extra components, changed name and version

--- charts/digitalhub/confs/dashboard/env.js
+++ charts/digitalhub/confs/dashboard/env.js
@@ -1,5 +1,5 @@
 {{ if .Values.dashboard.oidc.enabled -}}
-window.env = {"VITE_OIDC_CONFIG":JSON.stringify({"accessTokenExpiringNotificationTime": "3570", "authority": "{{ .Values.dashboard.oidc.config.issuer }}", "clientId": "{{ .Values.dashboard.oidc.audience.clientId }}", "redirectUri": "http://{{ include "digitalhub.oidcDashboardEndpoint" . }}/oidc-callback", "responseType": "code", "scope": "openid profile email", "automaticSilentRenew": "false", "automaticSilentSignin": "false", "post_logout_redirect_uri": "http://{{ include "digitalhub.oidcDashboardEndpoint" . }}"}), "VITE_PLATFORM_TITLE": "Digital Hub", "VITE_PLATFORM_VERSION": "0.4"}
+window.env = {"VITE_OIDC_CONFIG":JSON.stringify({"accessTokenExpiringNotificationTime": "3570", "authority": "{{ .Values.dashboard.oidc.config.issuer }}", "clientId": "{{ .Values.dashboard.oidc.audience.clientId }}", "redirectUri": "http://{{ include "digitalhub.oidcDashboardEndpoint" . }}/oidc-callback", "responseType": "code", "scope": "openid profile email", "automaticSilentRenew": "false", "automaticSilentSignin": "false", "post_logout_redirect_uri": "http://{{ include "digitalhub.oidcDashboardEndpoint" . }}"}), "VITE_PLATFORM_TITLE": "OltreAI", "VITE_PLATFORM_VERSION": "0.6"}
 {{- else -}}
-window.env = {"VITE_OIDC_CONFIG": null, "VITE_PLATFORM_TITLE": "Digital Hub", "VITE_PLATFORM_VERSION": "0.4"}
+window.env = {"VITE_OIDC_CONFIG": null, "VITE_PLATFORM_TITLE": "OltreAI", "VITE_PLATFORM_VERSION": "0.6"}
 {{- end }}
