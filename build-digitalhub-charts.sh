6ed9db01a9b69474ec7b97db07cb554de1d4d4c1
ffais <ffais@fbk.eu>
2024-09-09 10:19:51 +0200

    fix(core): fix missing quotes in core configmap
    Signed-off-by: ffais <ffais@fbk.eu>

--- build-digitalhub-charts.sh
+++ build-digitalhub-charts.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 build_chart () {
-  array=('aac' 'digitalhub' 'digitalhub-crds' 'mlrun' 'nakamasato-mysql-operator')
+  array=('aac' 'digitalhub' 'digitalhub-crds' 'mlrun' 'nakamasato-mysql-operator' 'mlrun-ce')
   for f in charts/*
   do
   IFS='/'
   read -ra newarr <<< "$f"
   chart="${newarr[1]}"
     if ! [[ ${array[*]} =~ $chart ]]; then 
+      echo "Packing $f)"
       helm package "$f" -d charts/digitalhub/charts
     fi
   done
