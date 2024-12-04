7ff23c123243b105ac461858cfd395c716df2828
Calcagiara <jacopocristoforitn@gmail.com>
2024-12-04 16:46:48 +0100

    feat(test): made platform tests usable for all environments, adding configuration options

--- charts/digitalhub/confs/tests/init-python-script.sh
+++ charts/digitalhub/confs/tests/init-python-script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cp home/src/$TEST_SCRIPT home/git/digitalhub-tutorials/$TEST_FOLDER/$TEST_SCRIPT
 cd home/git/digitalhub-tutorials/$TEST_FOLDER
-pip install --cache-dir home/pipcache digitalhub[full] digitalhub-runtime-python digitalhub-runtime-container digitalhub-runtime-dbt digitalhub-runtime-kfp digitalhub-runtime-modelserve requests-oauthlib
+{{- include "digitalhub.testPackages" . }}
 python $TEST_SCRIPT
